902b596de027562c25bae7a6ae798878d708c5b0
Prince Prem <153889186+Princeprem117@users.noreply.github.com>
2026-07-23 11:23:11 +0530

    Time: 3 ms (34.18%), Space: 19.4 MB (5.99%) - LeetHub

---
+++ 0202-happy-number/0202-happy-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        s= set()
+        while n!=1 and n not in s:
+            s.add(n)
+            t=0
+            
+            while n >0:
+                d=n%10
+                t+=d*d
+                n//=10
+            n=t
+        return n==1
